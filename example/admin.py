e1b9bef948d6352e57de7d3a12ccb5cb64aaf685
Francisco <franciscoigor@gmail.com>
2018-12-09 00:18:28 -0800

    Added models and integrated in admin panel

--- example/admin.py
+++ example/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Sender
+from .models import Message
+
+admin.site.register(Sender)
+admin.site.register(Message)
--- webapp/settings.py
+++ webapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'example',
+    'example.apps.ExampleConfig',
 ]
 
 MIDDLEWARE = [
