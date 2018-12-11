587be7dfa3c6df2deaf9671e5ef880b3a61dc5ce
Francisco <franciscoigor@gmail.com>
2018-12-10 16:04:18 -0800

    Modify main view with Message list

--- example/views.py
+++ example/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from example.models import Sender, Message
 
 def index(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    messages = Message.objects.all()
+    content = []
+    for message in messages:
+        content.append('<div style="border:1px solid gray;padding:5px">')
+        content.append('<div><b>From:</b> %s </div>' % message.sender )
+        content.append('<div><b>To:</b> %s </div>' % message.recipient )
+        content.append('<div><b>Date:</b> %s </div>' % message.timestamp )
+        content.append('<pre>%s</pre>' % message.message )
+        content.append('</div>')
+    return HttpResponse('<h1>Messages</h1> %s' % ('\n'.join(content) ) )
