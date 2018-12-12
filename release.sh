2476333435bc121d6b925be536475640c44d00c5
Francisco <franciscoigor@gmail.com>
2018-12-11 18:29:41 -0800

    create_user script

--- release.sh
+++ release.sh
@@ -1,5 +1,4 @@
-rm ./db.sqlite3
 python manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'Admin.123456')" | python manage.py shell
+cat create_user.py | python manage.py shell
 
 
