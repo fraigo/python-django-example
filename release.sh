c36645d7bd030c335b45f6a3eab6876338db8f3c
Francisco <franciscoigor@gmail.com>
2018-12-11 18:18:59 -0800

    Added release.sh

---
+++ release.sh
@@ -0,0 +1,4 @@
+python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'Admin.123456')" | python manage.py shell
+
+
