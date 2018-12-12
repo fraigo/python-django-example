33d6edc45d971b6928890ad28930e22e76832689
Francisco <franciscoigor@gmail.com>
2018-12-11 18:26:44 -0800

    added reset SQLite

--- release.sh
+++ release.sh
@@ -1,3 +1,4 @@
+rm ./db.sqlite3
 python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'Admin.123456')" | python manage.py shell
 
