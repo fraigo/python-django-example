fb25d97bb0e571b9ce746b5baa76a804d075e29b
Francisco <franciscoigor@gmail.com>
2018-12-12 17:00:54 -0800

    save data

--- create_data.py
+++ create_data.py
@@ -2,10 +2,13 @@ import datetime
 from example.models import Message, Sender
 
 sender = Sender(name="Me Myself", email="me@myself.com", imageUrl="")
+sender.save()
 recipient = Sender(name="You Yourseld", email="you@yourself.com", imageUrl="")
+recipient.save()
 message = Message(
     sender=sender,
     recipient=recipient,
     message="Hello! How are you?",
     timestamp=datetime.datetime.now()
     )
+message.save()
